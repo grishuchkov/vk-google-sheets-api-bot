d578404b6d2eff2412f146dbec6711a9aefc8fcd
grishuchkov <gri.d.a@ya.ru>
2023-10-18 13:39:39 +0300

    add: processMessageEvent

--- src/main/java/ru/grishuchkov/vkgooglesheetsapibot/service/VkCallbackService.java
+++ src/main/java/ru/grishuchkov/vkgooglesheetsapibot/service/VkCallbackService.java
@@ -1,12 +1,14 @@
 package ru.grishuchkov.vkgooglesheetsapibot.service;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.vk.api.sdk.objects.messages.Keyboard;
 import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
 import org.springframework.stereotype.Service;
 import ru.grishuchkov.vkgooglesheetsapibot.client.VkApiClient;
+import ru.grishuchkov.vkgooglesheetsapibot.dto.callback.MessageEventRequest;
 import ru.grishuchkov.vkgooglesheetsapibot.dto.callback.MessageRequest;
 import ru.grishuchkov.vkgooglesheetsapibot.utils.CallbackRequestMapper;
+import ru.grishuchkov.vkgooglesheetsapibot.utils.KeyboardUtil;
 
 import java.util.ResourceBundle;
 
@@ -17,6 +19,8 @@ public class VkCallbackService implements CallbackService {
     private final CallbackRequestMapper callbackRequestMapper;
     private final ResourceBundle messagesResource;
     private final VkApiClient vkClient;
+    private final KeyboardUtil keyboardUtil;
+
     @Override
     public String getConfirmationCode(JsonNode json) {
         int groupId = json.get("group_id").asInt();
@@ -31,9 +35,11 @@ public class VkCallbackService implements CallbackService {
         if (typeOfRequest.equals("message_new")) {
             processMessage(json);
         }
+        if (typeOfRequest.equals("message_event")) {
+            processMessageEvent(json);
+        }
     }
 
-    @SneakyThrows
     private void processMessage(JsonNode json) {
         MessageRequest request = callbackRequestMapper.mapMessageNew(json);
 
@@ -41,12 +47,29 @@ public class VkCallbackService implements CallbackService {
         int userId = request.getUserId();
         int groupId = request.getGroupId();
 
-        if (isCommand(messageText, "homework_keyboard_command")) {
-            vkClient.sendMessage(groupId, messagesResource.getString("homework_received"), userId);
+        if (isMessageCommand(messageText, "homework_command")) {
+            vkClient.sendMessage(groupId, userId, messagesResource.getString("homework_received"));
         }
+        if (isMessageCommand(messageText, "homework_keyboard_command")) {
+            Keyboard homeworkKeyboard = keyboardUtil.getHomeworkKeyboard();
+
+            vkClient.sendMessage(groupId, userId, messagesResource.getString("homework_keyboard_received"), homeworkKeyboard);
+        }
+
     }
 
-    private boolean isCommand(String messageText, String command) {
+    private void processMessageEvent(JsonNode jsonNode) {
+        MessageEventRequest request = callbackRequestMapper.mapMessageEvent(jsonNode);
+
+        int userId = request.getUserId();
+        int groupId = request.getGroupId();
+        int peerId = request.getPeerId();
+        String eventId = request.getObjectEventId();
+
+        vkClient.sendMessageEventAnswer(groupId, userId, peerId, eventId);
+    }
+
+    private boolean isMessageCommand(String messageText, String command) {
         return messageText.equalsIgnoreCase(messagesResource.getString(command));
     }
 }
